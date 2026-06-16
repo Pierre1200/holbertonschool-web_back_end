cdea2c16d3925e0718a81aa1a0c2210c709101ed
Pierre1200 <rouvellat.p@icloud.com>
2026-06-16 13:21:27 +0200

    Refactor task_wait_n to define tasks list earlier in the function

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -16,6 +16,7 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     au cycle de complétion asynchrone.
     """
     delays: List[float] = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
 
     for task in asyncio.as_completed(tasks):
         delay = await task
