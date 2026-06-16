c483a4c064f32bb79c7de61221adf19d23c65d9f
Pierre1200 <rouvellat.p@icloud.com>
2026-06-16 11:47:28 +0200

    Add implementation of wait_n coroutine for concurrent execution

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module for basic async syntax
+"""
+import asyncio
+from typing import list
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Lance n fois la coroutine wait_random en même temps et retourne
+    la liste des délais générés, triée de manière croissante grâce
+    au cycle de complétion asynchrone.
+    """
+    delays: List[float] = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
